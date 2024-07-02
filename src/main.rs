b68223b0fcde49509b3ab9c06ccd07883febba5b
18-RAJAT <Rajatjoshi7535@gmail.com>
2024-07-02 11:04:36 +0530

    Binding

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,34 @@
-use ferris_says::say;
-use std::io::{stdout,BufWriter};
+// use ferris_says::say;
+// use std::io::{stdout,BufWriter};
+
+// fn main()
+// {
+//     let stdout=stdout();
+//     let message=String::from("Hello Rust");
+//     let width=message.chars().count();
+
+//     let mut writer=BufWriter::new(stdout.lock());
+//     say(message.as_bytes(),width,&mut writer).unwrap();
+// }
+
+
+
+
+
+
+
+//Binding
 
 fn main()
 {
-    let stdout=stdout();
-    let message=String::from("Hello Rust");
-    let width=message.chars().count();
+    let x:i32;//type
+    println!("Success");
 
-    let mut writer=BufWriter::new(stdout.lock());
-    say(message.as_bytes(),width,&mut writer).unwrap();
+    mutable
+    let mut x:i32;
+    x=1;
+    x+=2;
+    assert_eq!(x,3);
+    println!("Success");
 }
+
